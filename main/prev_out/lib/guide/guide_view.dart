5a2688ef5080ea54fa3681e494670b1126a16c82
Luis Mayorga <eduardo.mayorgab@gmail.com>
2024-07-31 19:48:15 -0600

    actualización de archivos

--- main/prev_out/lib/guide/guide_view.dart
+++ main/prev_out/lib/guide/guide_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:prev_out/appbar.dart';
+import 'package:flutter/services.dart';
 
 class GuideApp extends StatelessWidget {
   const GuideApp({super.key});
@@ -28,18 +29,20 @@ class _CombinedWidgetState extends State<CombinedWidget> {
       appBar: const CustomAppBar(),
       body: ListView(
         children: [
-          contenido(),
+          contenido(context),
         ],
       ),
     );
   }
+}
 
-  Widget contenido() {
+
+  Widget contenido(BuildContext context) {
     return Center(
       child: Column(
         children: <Widget>[
           checklist(),
-          lista(),
+          lista(context),
         ],
       ),
     );
@@ -73,35 +76,97 @@ class _CombinedWidgetState extends State<CombinedWidget> {
     );
   }
 
-  Widget lista() {
-    return Container(
-      color: const Color(0xffFFE5E5), // Color de fondo de toda la página
-      margin: const EdgeInsets.all(8),
-      child: Center(
-        child: Container(
-          width: double.infinity, // Ocupar todo el espacio horizontal
-          height: MediaQuery.of(context)
-              .size
-              .height, // Ocupar todo el espacio vertical
-          decoration: BoxDecoration(
-            color: Colors.white, // Color del card
-            borderRadius: BorderRadius.circular(13.0), // Bordes redondeados
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.withOpacity(0.5),
-                spreadRadius: 3,
-                blurRadius: 5,
-                offset: const Offset(0, 3), // Cambia la posición de la sombra
-              ),
-            ],
-          ),
-          child: const Padding(
-            padding: EdgeInsets.all(30.0),
-            child: Text(
-              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas interdum, erat at pulvinar facilisis, mauris velit ullamcorper sem, eu commodo leo est ac ipsum. Mauris et justo condimentum urna iaculis tempor. Sed pretium porta sem id bibendum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Morbi tincidunt purus porttitor, aliquam justo sed, vehicula turpis. Suspendisse vitae tincidunt diam, et iaculis velit. Nam at nisi odio. Quisque laoreet accumsan fringilla. Integer rutrum ipsum condimentum, elementum metus non, commodo libero. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas interdum, erat at pulvinar facilisis, mauris velit ullamcorper sem, eu commodo leo est ac ipsum. Mauris et justo condimentum urna iaculis tempor. Sed pretium porta sem id bibendum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Morbi tincidunt purus porttitor, aliquam justo sed, vehicula turpis. Suspendisse vitae tincidunt diam, et iaculis velit. Nam at nisi odio. Quisque laoreet accumsan fringilla. Integer rutrum ipsum condimentum, elementum metus non, commodo libero. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas interdum, erat at pulvinar facilisis, mauris velit ullamcorper sem, eu commodo leo est ac ipsum. Mauris et justo condimentum urna iaculis tempor. Sed pretium porta sem id bibendum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Morbi tincidunt purus porttitor, aliquam justo sed, vehicula turpis. Suspendisse vitae tincidunt diam, et iaculis velit. Nam at nisi odio. Quisque laoreet accumsan fringilla. Integer rutrum ipsum condimentum, elementum metus non, commodo libero.',
-              style: TextStyle(fontSize: 15),
+Widget lista(BuildContext context) {
+  return Container(
+    color: const Color(0xffFFE5E5), // Color de fondo de toda la página
+    margin: const EdgeInsets.all(8),
+    child: Center(
+      child: Container(
+        width: double.infinity, // Ocupar todo el espacio horizontal
+        height: MediaQuery.of(context).size.height, // Ocupar todo el espacio vertical
+        decoration: BoxDecoration(
+          color: Colors.white, // Color del card
+          borderRadius: BorderRadius.circular(13.0), // Bordes redondeados
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 3,
+              blurRadius: 5,
+              offset: const Offset(0, 3), // Cambia la posición de la sombra
+            ),
+          ],
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(30.0),
+          child: Center(
+            child: CodeBox(
+              code: '''
+  void main() {
+  print('Hola, Mundo!')
+                  =
+                  =
+                  =
+                  =
+                  0
+                  0
+                  0
+                  0
+                  0
+                  
+                  0
+                  0;
+                }
+              ''',
             ),
           ),
+        ),
+      ),
+    ),
+  );
+}
+
+class CodeBox extends StatelessWidget {
+  final String code;
+
+  CodeBox({required this.code});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 200.0, // Ajusta el ancho según lo necesites
+      height: 200.0, // Ajusta la altura según lo necesites
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(8.0),
+          color: Colors.grey[200],
+        ),
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: SingleChildScrollView(
+                child: SelectableText(
+                  code,
+                  style: TextStyle(
+                    fontFamily: 'monospace',
+                    fontSize: 14.0,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 8.0),
+            ElevatedButton(
+              onPressed: () {
+                Clipboard.setData(ClipboardData(text: code));
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('Código copiado al portapapeles')),
+                );
+              },
+              child: Text('Copiar Código'),
+            ),
+          ],
         ),
       ),
     );
