1a3ba38b304cf4d6a65d7cd2055d766ffba8fd08
Dealo Arroyo <dealoarroyo@MacBook-Air-de-Dealo.local>
2024-10-14 14:46:15 -0600

    Haciendo el manual de uso

--- main/prev_out/lib/guide/guide_view.dart
+++ main/prev_out/lib/guide/guide_view.dart
@@ -61,9 +61,10 @@ class _CombinedWidgetState extends State<CombinedWidget> {
                 child: Text(
                   'Manual de uso',
                   style: TextStyle(
-                      fontSize: 18,
-                      color: Color(0xff000000),
-                      fontWeight: FontWeight.bold),
+                    fontSize: 18,
+                    color: Color(0xff000000),
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
             ],
@@ -78,31 +79,88 @@ class _CombinedWidgetState extends State<CombinedWidget> {
       color: const Color(0xffFFE5E5), // Color de fondo de toda la página
       margin: const EdgeInsets.all(8),
       child: Center(
-        child: Container(
-          width: double.infinity, // Ocupar todo el espacio horizontal
-          height: MediaQuery.of(context)
-              .size
-              .height, // Ocupar todo el espacio vertical
-          decoration: BoxDecoration(
-            color: Colors.white, // Color del card
-            borderRadius: BorderRadius.circular(13.0), // Bordes redondeados
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.withOpacity(0.5),
-                spreadRadius: 3,
-                blurRadius: 5,
-                offset: const Offset(0, 3), // Cambia la posición de la sombra
+        child: Stack(
+          children: [
+            Container(
+              width: double.infinity,
+              height: MediaQuery.of(context)
+                  .size
+                  .height, // Ocupar todo el espacio vertical
+              decoration: BoxDecoration(
+                color: Colors.white, // Color del card
+                borderRadius: BorderRadius.circular(13.0), // Bordes redondeados
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 3,
+                    blurRadius: 5,
+                    offset:
+                        const Offset(0, 3), // Cambia la posición de la sombra
+                  ),
+                ],
               ),
-            ],
-          ),
-          child: const Padding(
-            padding: EdgeInsets.all(30.0),
-            child: Text(
-              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas interdum, erat at pulvinar facilisis, mauris velit ullamcorper sem, eu commodo leo est ac ipsum. Mauris et justo condimentum urna iaculis tempor. Sed pretium porta sem id bibendum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Morbi tincidunt purus porttitor, aliquam justo sed, vehicula turpis. Suspendisse vitae tincidunt diam, et iaculis velit. Nam at nisi odio. Quisque laoreet accumsan fringilla. Integer rutrum ipsum condimentum, elementum metus non, commodo libero. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas interdum, erat at pulvinar facilisis, mauris velit ullamcorper sem, eu commodo leo est ac ipsum. Mauris et justo condimentum urna iaculis tempor. Sed pretium porta sem id bibendum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Morbi tincidunt purus porttitor, aliquam justo sed, vehicula turpis. Suspendisse vitae tincidunt diam, et iaculis velit. Nam at nisi odio. Quisque laoreet accumsan fringilla. Integer rutrum ipsum condimentum, elementum metus non, commodo libero. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas interdum, erat at pulvinar facilisis, mauris velit ullamcorper sem, eu commodo leo est ac ipsum. Mauris et justo condimentum urna iaculis tempor. Sed pretium porta sem id bibendum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Morbi tincidunt purus porttitor, aliquam justo sed, vehicula turpis. Suspendisse vitae tincidunt diam, et iaculis velit. Nam at nisi odio. Quisque laoreet accumsan fringilla. Integer rutrum ipsum condimentum, elementum metus non, commodo libero.',
-              style: TextStyle(fontSize: 15),
+              child: const Padding(
+                padding: EdgeInsets.only(left: 80, right: 800, top: 100),
+                child: Column(
+                  crossAxisAlignment:
+                      CrossAxisAlignment.start, // Alineación a la izquierda
+                  children: [
+                    Text(
+                      'Pantalla de inicio',
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 20), // Añade espacio entre los textos
+                    Text(
+                      'La aplicación nos recibe con un inicio de sesión. El perfil del Administrador tiene la función de que puede agregar un nuevo usuario o puede borrarlos. En el perfil de Usuario, por ahora, tiene todas las funcionalidades del Administrador, salvo que no puede agregar o eliminar usuarios.',
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
-          ),
+            Positioned(
+              top: 50,
+              right: 90,
+              child: imagen(), // Aquí se inserta la imagen
+            ),
+          ],
         ),
+      ),
+    );
+  }
+
+  Widget imagen() {
+    return Container(
+      width: 700,
+      height: 300,
+      alignment: Alignment.topRight,
+      child: Image.asset(
+        'assets/images/foto_manual_1.webp',
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+
+  Widget titleHomeScreen() {
+    return const Text(
+      'Pantalla de inicio',
+      style: TextStyle(
+        fontSize: 25,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+
+  Widget textHomeScreen() {
+    return const Text(
+      'La aplicación nos recibe con un inicio de sesión. El perfil del Administrador tiene la función de que puede agregar un nuevo usuario o puede borrarlos. En el perfil de Usuario, por ahora, tiene todas las funcionalidades del Administrador, salvo que no puede agregar o eliminar usuarios.',
+      style: TextStyle(
+        fontSize: 18,
       ),
     );
   }
