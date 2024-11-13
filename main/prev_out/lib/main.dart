51f9c65b0105743bfc26440c89326f40925ee8ba
Dealo Arroyo <dealoarroyo@MacBook-Air-de-Dealo.local>
2024-11-13 08:32:46 -0600

    N/A

--- main/prev_out/lib/main.dart
+++ main/prev_out/lib/main.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
 import 'login/widgets/login_page.dart';
 import 'home/home_page.dart';
+import 'package:desktop_window/desktop_window.dart';
 
-void main() => runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await testWindowSize();
+  runApp(const MyApp());
+}
+
+Future<void> testWindowSize() async {
+  await DesktopWindow.setMaxWindowSize(const Size(1460, 840));
+  await DesktopWindow.setMinWindowSize(const Size(1460, 840));
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
